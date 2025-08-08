4ec475cc038375901a0383baf655f6a7225b0aca
Disha Kemble <disha.kemble2601@gmail.com>
2025-08-08 16:06:45 +0530

    Delete  directory

--- backend_git/dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwt_secret = exports.connectionDB = void 0;
-exports.connectionDB = "mongodb+srv://dishakemble58513:OlJ1Kh7B02TFnVeQ@cluster0.h2tlxyv.mongodb.net/BrainBoard";
-exports.jwt_secret = "fbufefbhfbyfghbfhfrhbf";
